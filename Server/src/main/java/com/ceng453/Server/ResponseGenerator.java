e0bd6ae25072996558015554aae9ab17c1101875
Mert Tunc <mert.tunc@udemy.com>
2018-10-25 14:11:28 +0300

    Working Sprintboot REST framework

---
+++ Server/src/main/java/com/ceng453/Server/ResponseGenerator.java
@@ -0,0 +1,13 @@
+package com.ceng453.Server;
+
+public class ResponseGenerator {
+    private final String name;
+
+    public ResponseGenerator(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- Server/src/main/java/com/ceng453/Server/ServerApplication.java
+++ Server/src/main/java/com/ceng453/Server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.ceng453.Server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
