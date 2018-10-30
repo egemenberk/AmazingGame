1a8151e5abc9dfdb9e4c1df9b7a3b18b63976ccc
Mert Tunc <mert.tunc@udemy.com>
2018-10-30 18:48:35 +0300

    Added base game classes and interfaces

--- Server/src/main/java/com/ceng453/Server/SinglePlayerGameInstance.java
+++ Server/src/main/java/com/ceng453/Server/SinglePlayerGameInstance.java
@@ -1,22 +1,80 @@
 package com.ceng453.Server;
 
+
+import javax.json.Json;
+import java.util.ArrayList;
+
 public class SinglePlayerGameInstance implements GameInstance {
     private String assignedUserToken;
     private Thread workerThread;
+    private Object responseGenerationLock;
+    private String responseAsJson;
+    private boolean terminateSignal;
+    private long currentGameCycle;
+
+    private ArrayList<GameObject> alienShips;
+    private GameObject playerShip;
+    private ArrayList<GameObject> bullets;
 
 
     public SinglePlayerGameInstance(String tokenOfUser) {
         assignedUserToken = tokenOfUser;
-        workerThread = new Thread( this );
-    }
 
-    @Override
-    public void StartGame() {
-        workerThread.start();
+        workerThread = new Thread( this );
+
+        currentGameCycle = 0;
+
     }
 
     @Override
     public void run() {
         System.out.println("Thread started for user " + assignedUserToken);
+
+        while(!terminateSignal){
+            currentGameCycle++;
+
+            ProcessUserInput(); // read user input
+            UpdateGameState();// update game
+            serialize();
+
+            try {
+                Thread.sleep(30); // TODO adjust to keep fps stable
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void ProcessUserInput(){
+
+    }
+
+    private void UpdateGameState(){
+        for( GameObject alienShip : alienShips ){
+            alienShip.update(currentGameCycle);
+        }
+    }
+
+    private String serialize(){
+        synchronized (responseGenerationLock) {
+            String responseAsJson = Json.createObjectBuilder().add("key", "value").build().toString();
+
+            return responseAsJson;
+        }
+    }
+
+    public String GetGameStateAsJson(){
+        synchronized (responseGenerationLock){
+            return responseAsJson;
+        }
+    }
+
+    public void setTerminateSignal(boolean terminateSignal) {
+        this.terminateSignal = terminateSignal;
+    }
+
+    @Override
+    public void StartGame() {
+        workerThread.start();
     }
 }
