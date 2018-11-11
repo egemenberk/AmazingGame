2567a1f211e74d6afe78dcb2ec96f849bcdb45e2
axydor <e2099018@ceng.metu.edu.tr>
2018-11-11 12:29:09 +0300

    Delete Mapping is corrected

--- Server/src/main/java/com/ceng453/Server/UserRepository.java
+++ Server/src/main/java/com/ceng453/Server/UserRepository.java
@@ -2,6 +2,8 @@ package com.ceng453.Server;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 
@@ -9,6 +11,8 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer>, UserRepositoryCustom {
 
     List<User> findBySession(String token);
+
+    @Transactional
     void deleteBySession(String session);
 }
 
