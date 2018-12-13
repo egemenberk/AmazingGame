2af3812f2d46eae350983298671ea13240657168
Mert Tunc <mert.tunc@udemy.com>
2018-12-13 22:34:45 +0300

    Changed a debug line

--- Frontend/src/main/com/ceng453/frontend/GameService.java
+++ Frontend/src/main/com/ceng453/frontend/GameService.java
@@ -39,9 +39,9 @@ public class GameService {
     public GameService(String userAuthToken) {
         levels = new LinkedList<>();
 
-        //levels.push(new GameLevel2());
-        //levels.push(new GameLevel1());
         levels.push(new GameLevel3());
+        levels.push(new GameLevel2());
+        levels.push(new GameLevel1());
 
         this.userAuthToken = userAuthToken;
     }
